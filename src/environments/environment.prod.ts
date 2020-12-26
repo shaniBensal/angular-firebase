24e9f21f944f4a13a8509dbe178cc86cd6cda294
Shani <shano.bensal@mail.huji.ac.il>
2020-12-26 18:38:49 +0200

    before-deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCG334GLQAk1_dzSp98DpPzkLXOLiekoow",
+    authDomain: "realcommerce-4c9bb.firebaseapp.com",
+    projectId: "realcommerce-4c9bb",
+    storageBucket: "realcommerce-4c9bb.appspot.com",
+    messagingSenderId: "106253756909",
+    appId: "1:106253756909:web:0a95b32278c7201787dfe4",
+    measurementId: "G-9WG40DSRY5",
+    databaseURL: "https://realcommerce-4c9bb-default-rtdb.firebaseio.com",
+  }
 };
